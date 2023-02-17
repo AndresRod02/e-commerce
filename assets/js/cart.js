2b76f17e4bd1827afd48871365e40915270fefa6
Andrés Rodríguez <artdavid2022@gmail.com>
2023-02-16 23:17:54 -0500

    Limpiando

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -132,10 +132,10 @@ const db = [
   }
 ];
 
-const products = window.localStorage.getItem("productsDB")
-? JSON.parse(window.localStorage.getItem("productsDB"))
-: db;
-// const products = db;
+// const products = window.localStorage.getItem("productsDB")
+// ? JSON.parse(window.localStorage.getItem("productsDB"))
+// : db;
+const products = db;
 
 // #2 Pintar los productos en el DOM
 const productContainer = document.getElementById("products__content");
